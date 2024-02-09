81b6f75ca4920442fc9b037e2dc0d2ba23a03786
daviekim <daviekim.dev@gmail.com>
2024-02-09 04:58:31 -0600

    modifies skillvsme button to fit the design given

--- app/src/main/java/s/skillvsme/presentation/components/SkillvsmeRadioBtn.kt
+++ app/src/main/java/s/skillvsme/presentation/components/SkillvsmeRadioBtn.kt
@@ -2,40 +2,55 @@ package s.skillvsme.presentation.components
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.RadioButton
+import androidx.compose.material.RadioButtonDefaults
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import s.skillvsme.ui.theme.black
+import s.skillvsme.ui.theme.white
 
 @Composable
 fun SkillvsmeRadioBtn(
+    modifier: Modifier,
     selectedValue: String,
     label: String,
     onClick: () -> Unit = {}
 ) {
     val primary = if (selectedValue == label) true else false
-    Row(
-        verticalAlignment = Alignment.CenterVertically,
-        modifier = Modifier.background(
-            color = if (primary) MaterialTheme.colors.primary else MaterialTheme.colors.onPrimary
-        ).padding(20.dp)
+    BorderedSurface(
+        modifier = modifier,
+        borderColor = if (primary) white else black,
+        borderWidth = 1.dp,
+        cornerRadius = 24.dp
     ) {
         Row(
-            horizontalArrangement = Arrangement.Start
+            horizontalArrangement = Arrangement.Start,
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.background(
+                color = if (primary) black else white
+            ).padding(8.dp),
         ){
             RadioButton(
                 selected = selectedValue == label,
-                onClick = onClick
+                onClick = onClick,
+                colors = RadioButtonDefaults.colors(
+                    selectedColor = white,
+                    unselectedColor = black
+                )
             )
             Text(
                 text = label,
-                color = if (primary) MaterialTheme.colors.onPrimary else MaterialTheme.colors.primary,
+                color = if (primary) white else black,
                 modifier = Modifier.fillMaxWidth()
             )
         }
