b1aa2b69c81cb70b8b558b36b9f872706b345c79
daviekim <daviekim.dev@gmail.com>
2024-02-08 23:58:29 -0600

    adds text component that ends with a link

--- app/src/main/java/s/skillvsme/presentation/onboarding/TextEndingWithLink.kt
+++ app/src/main/java/s/skillvsme/presentation/onboarding/TextEndingWithLink.kt
@@ -1,4 +1,52 @@
 package s.skillvsme.presentation.onboarding
 
-class TextEndingWithLink {
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import s.skillvsme.ui.theme.purple
+
+
+@Composable
+fun TextEndingWithLink(
+    modifier: Modifier = Modifier.fillMaxSize(),
+    text: String,
+    linkText: String,
+    onLinkClick: () -> Unit
+) {
+    Row(
+        modifier = modifier,
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Center
+    ) {
+        Text(text = text)
+        Spacer(modifier = Modifier.width(8.dp))
+        Text(
+            text = linkText,
+            color = purple,
+            modifier = Modifier
+                .clickable(onClick = onLinkClick)
+                .drawBehind {
+                    val strokeWidthPx = 1.dp.toPx()
+                    val verticalOffset = size.height - 2.sp.toPx()
+                    drawLine(
+                        color = purple,
+                        strokeWidth = strokeWidthPx,
+                        start = Offset(0f, verticalOffset),
+                        end = Offset(size.width, verticalOffset)
+                    )
+                }
+        )
+    }
 }
