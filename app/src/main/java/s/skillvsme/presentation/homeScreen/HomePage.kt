1f06dad8207d026d2108ac7253ff407d9c43925f
daviekimgithub <ericokip402@gmail.com>
2024-02-09 12:14:50 +0300

    implementing the home pages

--- app/src/main/java/s/skillvsme/presentation/homeScreen/HomePage.kt
+++ app/src/main/java/s/skillvsme/presentation/homeScreen/HomePage.kt
@@ -65,7 +65,7 @@ fun HomePage(
                     ) {
                         // Image
 
-                        Image(painter =(painterResource(id =R.drawable.account )) ,
+                        Image(painter =(painterResource(id =R.drawable.profile )) ,
                             contentDescription ="",Modifier.fillMaxSize(),
                             contentScale = ContentScale.Crop )
                     }
@@ -147,6 +147,7 @@ fun HomePage(
                                             .background(Color.White, CircleShape),
                                         contentAlignment = Alignment.BottomCenter
                                     ) {
+                                        Image(painter = painterResource(id = R.drawable.profile), contentDescription ="", contentScale = ContentScale.Crop )
                                         Box(
                                             modifier = Modifier
                                                 .graphicsLayer {
@@ -305,15 +306,16 @@ fun HomePage(
                                     Spacer(modifier = Modifier.width(10.dp))
                                     Box(
                                         modifier = Modifier
-                                            .size(70.dp)
+                                            .size(50.dp)
                                             .background(Color.White, CircleShape)
                                     ) {
                                         // Image
+                                        Image(painter = painterResource(id = R.drawable.profi), contentDescription = "", contentScale = ContentScale.Crop)
                                     }
 
                                 }
                             }
-                            Spacer(modifier = Modifier.width(3.dp))
+                            Spacer(modifier = Modifier.width(10.dp))
                         }
                     }
                 }
