22177492699b0fe5449a34371cf93b22eef4580c
daviekim <daviekim.dev@gmail.com>
2024-02-09 00:30:57 -0600

    adds a code verification screen

--- app/src/main/java/s/skillvsme/presentation/onboarding/CodeVerification.kt
+++ app/src/main/java/s/skillvsme/presentation/onboarding/CodeVerification.kt
@@ -1,2 +1,160 @@
 package s.skillvsme.presentation.onboarding
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.geometry.CornerRadius
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import s.skillvsme.R
+import s.skillvsme.presentation.components.SkillvsmeButton
+import s.skillvsme.presentation.components.SkillvsmeText
+import s.skillvsme.presentation.components.SkillvsmeTextField
+import s.skillvsme.ui.theme.black
+import s.skillvsme.ui.theme.lightGrey
+import s.skillvsme.ui.theme.white
+
+
+@Composable
+fun CodeVerification() {
+    val scrollState = rememberScrollState()
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .verticalScroll(state = scrollState)
+            .background(white,)
+    ){
+        Column(
+            verticalArrangement = Arrangement.SpaceBetween,
+            modifier = Modifier
+                .fillMaxWidth()
+        ) {
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(Color(0XFFF2F3F4)),
+                contentAlignment = Alignment.TopCenter
+
+            ){
+                Image(
+                    painter = painterResource(id = R.drawable.top_blackish_bg),
+                    contentDescription = "Vehicles Image",
+                    modifier    = Modifier
+                        .fillMaxWidth()
+                        .height(300.dp),
+                    contentScale = ContentScale.Crop
+                )
+                Column {
+                    Spacer(modifier = Modifier.height(50.dp))
+                    Image(
+                        painter = painterResource(id = R.drawable.logo),
+                        contentDescription = "logo",
+                        modifier = Modifier
+                            .size(100.dp)
+                            .graphicsLayer {
+                                this.scaleX = 1.5f
+                                this.scaleY = 1.5f
+                            }
+                    )
+                }
+                Row(
+                    modifier = Modifier
+                        .padding(4.dp)
+                        .fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    Box(
+                        modifier = Modifier
+                            .size(32.dp)
+                            .clip(RoundedCornerShape(10.dp)),
+                        contentAlignment = Alignment.Center
+                    ){
+                        Image(
+                            painter = painterResource(id = R.drawable.back_arrow_white),
+                            contentDescription = "back arrow",
+                            modifier = Modifier
+                                .size(16.dp)
+                                .graphicsLayer {
+                                    this.scaleX = 1.5f
+                                    this.scaleY = 1.5f
+                                }
+                        )
+                    }
+                }
+            }
+            Box(
+                modifier = Modifier
+                    .graphicsLayer {
+                        translationY = -100.dp.toPx()
+                    }
+            ){
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .drawBehind {
+                            drawRoundRect(
+                                white,
+                                cornerRadius = CornerRadius(48.dp.toPx())
+                            )
+                        }
+                ) {
+                    Row(
+                        modifier = Modifier
+                            .padding(20.dp)
+                            .fillMaxWidth(),
+                        horizontalArrangement = Arrangement.SpaceEvenly
+                    ) {
+                        SkillvsmeText(
+                            modifier = Modifier
+                                .padding(10.dp),
+                            label = "Verify Phone Number",
+                            value = "We have sent you a one time verification code on to +999XXXXXXX45 ",
+                            boldValue = true,
+                            valueColor = white
+                        )
+                    }
+                    Spacer(modifier = Modifier.height(16.dp))
+                    TextEndingWithLink(
+                        text = "Already have an account?",
+                        linkText = "login in",
+                        onLinkClick = {}
+                    )
+                    Spacer(modifier = Modifier.height(16.dp))
+                    SkillvsmeButton(
+                        label = "Continue",
+                        modifier = Modifier
+                            .padding(horizontal = 20.dp)
+                            .fillMaxWidth()
+                    ) {}
+                    Spacer(modifier = Modifier.height(16.dp))
+                }
+            }
+        }
+    }
+}
