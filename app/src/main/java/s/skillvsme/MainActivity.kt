8d767bdc014484ae6c6dd45b5f36eb58f621643d
daviekimgithub <ericokip402@gmail.com>
2024-02-10 20:46:39 +0300

    implementing the Profile, Edit profile Editing Language

--- app/src/main/java/s/skillvsme/MainActivity.kt
+++ app/src/main/java/s/skillvsme/MainActivity.kt
@@ -14,6 +14,7 @@ import androidx.navigation.compose.rememberNavController
 import s.skillvsme.common.Route
 import s.skillvsme.presentation.homeScreen.HomePage
 import s.skillvsme.presentation.homeScreen.HomeScreen
+import s.skillvsme.presentation.userprofilestudent.EditLanguage
 import s.skillvsme.presentation.userprofilestudent.EditProfile
 import s.skillvsme.presentation.userprofilestudent.UserProfile
 import s.skillvsme.ui.theme.SkillVsMeTheme
@@ -38,7 +39,7 @@ class MainActivity : ComponentActivity() {
                             startDestination = Route.Home.Home
                         ) {
                             composable(Route.Home.Home) {
-                                EditProfile()
+                                EditLanguage()
                             }
                             }
                         }
