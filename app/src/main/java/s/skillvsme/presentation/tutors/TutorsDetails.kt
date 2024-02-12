52089587736a97d3feb39c49ea3638156c4048b7
daviekim <daviekim.dev@gmail.com>
2024-02-11 21:51:19 -0600

    adds a tutors details screen

---
+++ app/src/main/java/s/skillvsme/presentation/tutors/TutorsDetails.kt
@@ -0,0 +1,11 @@
+package s.skillvsme.presentation.tutors
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+
+@Composable
+fun TutorsDetails() {
+    Column {
+        
+    }
+}
