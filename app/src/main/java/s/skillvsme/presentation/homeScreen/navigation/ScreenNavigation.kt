bb2c8d9cbe833456fbb196a7104ea0e9de300974
AriqKip <ericokip402@gmail.com>
2024-02-13 15:01:19 +0300

    implementing the routes for the student part on bottom navigation

--- app/src/main/java/s/skillvsme/presentation/homeScreen/navigation/ScreenNavigation.kt
+++ app/src/main/java/s/skillvsme/presentation/homeScreen/navigation/ScreenNavigation.kt
@@ -1,10 +1,10 @@
 package s.skillvsme.presentation.homeScreen.navigation
 import s.skillvsme.R
 
-sealed class ScreenNavigation (var title :String,var icon :Int,var screen_route :String){
-    object Home : ScreenNavigation("Home", R.drawable.home ,"/home")
-    object Tutor : ScreenNavigation("Tutor", R.drawable.teachersvg,"/students/details")
-    object Stream : ScreenNavigation("Stream",R.drawable.live_broadcasting,"/profile/edit")
-    object Classes : ScreenNavigation("Classes",R.drawable.baseline_home_24,"/profile/edit")
-    object Account : ScreenNavigation("Account",R.drawable.account,"/profile/edit")
+sealed class ScreenNavigation (var title :String,var icon :Int,var screenroute :String){
+    object Home : ScreenNavigation("Home", R.drawable.home ,"/student/home")
+    object Tutor : ScreenNavigation("Tutor", R.drawable.teachersvg,"/student/tutor")
+    object Stream : ScreenNavigation("Stream",R.drawable.live_broadcasting,"student/stream/")
+    object Classes : ScreenNavigation("Classes",R.drawable.baseline_home_24,"/student/classes")
+    object Account : ScreenNavigation("Account",R.drawable.account,"/student/profile")
 }
