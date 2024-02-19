6526614b052a2eb923d89c295f1a65477cca3561
daviekim <daviekim.dev@gmail.com>
2024-02-19 08:00:47 -0600

    adds bottom navigation bar in the confirmation success screen

--- app/src/main/java/s/skillvsme/presentation/tutors/AppointmentConfirmationSuccess.kt
+++ app/src/main/java/s/skillvsme/presentation/tutors/AppointmentConfirmationSuccess.kt
@@ -1,16 +1,35 @@
 package s.skillvsme.presentation.tutors
 
+import android.annotation.SuppressLint
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
 import s.skillvsme.presentation.components.SkillvsmeSuccessScreen
+import s.skillvsme.presentation.homeScreen.navigation.BottomNavigation
 
 
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@OptIn(ExperimentalMaterial3Api::class)
+@RequiresApi(Build.VERSION_CODES.Q)
 @Composable
-fun AppointmentConfirmationSuccess() {
-    SkillvsmeSuccessScreen(
-    successMessage = "Appointment Confirmed",
-    buttonText = "View all classes",
-    buttonOnclickAction = {},
-    backButtonText = "Back to home",
-    backButtonOnclickAction = {},
+fun AppointmentConfirmationSuccess(
+    navController: NavController
+) {
+    Scaffold(
+        content = {
+            SkillvsmeSuccessScreen(
+                successMessage = "Appointment Confirmed",
+                buttonText = "View all classes",
+                buttonOnclickAction = {},
+                backButtonText = "Back to home",
+                backButtonOnclickAction = {},
+            )
+        },
+        bottomBar = {
+            BottomNavigation(navController = navController)
+        }
     )
 }
