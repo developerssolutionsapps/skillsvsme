887a2162cef1c97c862644ce3e1ede716f562630
daviekim <daviekim.dev@gmail.com>
2024-02-19 09:29:37 -0600

    adds the two buttons in the bottom part of the tutors list screen

--- app/src/main/java/s/skillvsme/presentation/tutors/TutorsList.kt
+++ app/src/main/java/s/skillvsme/presentation/tutors/TutorsList.kt
@@ -43,7 +43,9 @@ import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import s.skillvsme.R
 import s.skillvsme.common.Fonts
+import s.skillvsme.common.Route
 import s.skillvsme.presentation.components.SimpleAppBar
+import s.skillvsme.presentation.components.SkillvsmeButton
 import s.skillvsme.presentation.components.TutorsListComponent
 import s.skillvsme.ui.theme.black
 import s.skillvsme.ui.theme.white
@@ -148,7 +150,25 @@ fun TutorsList(
                     color = black
                 )
                 Spacer(modifier = Modifier.height(8.dp))
-                Spacer(modifier = Modifier.height(40.dp))
+                Spacer(modifier = Modifier.height(8.dp))
+                SkillvsmeButton(
+                    modifier = Modifier.fillMaxWidth(),
+                    label = "Schedule a class",
+                    primary = true,
+                    onClick = {
+                        navController.navigate(Route.Student.Tutor.Schedule)
+                    }
+                )
+                Spacer(modifier = Modifier.height(4.dp))
+                SkillvsmeButton(
+                    modifier = Modifier.fillMaxWidth(),
+                    label = "Back",
+                    primary = false,
+                    onClick = {
+                        navController.popBackStack()
+                    }
+                )
+                Spacer(modifier = Modifier.height(48.dp))
             }
         },
         bottomBar = {
