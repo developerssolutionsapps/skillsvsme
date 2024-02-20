0df23a3a347e99920695ed8c46d45286fedd194a
daviekim <daviekim.dev@gmail.com>
2024-02-19 23:48:40 -0600

    links the payment pages together

--- app/src/main/java/s/skillvsme/presentation/payment/PaymentSuccess.kt
+++ app/src/main/java/s/skillvsme/presentation/payment/PaymentSuccess.kt
@@ -1,5 +1,8 @@
 package s.skillvsme.presentation.payment
 
+import android.annotation.SuppressLint
+import android.os.Build
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -9,6 +12,8 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -18,19 +23,44 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
 import s.skillvsme.R
+import s.skillvsme.common.Route
+import s.skillvsme.presentation.components.SimpleAppBar
 import s.skillvsme.presentation.components.SkillvsmeButton
 import s.skillvsme.presentation.components.SkillvsmeSuccessScreen
+import s.skillvsme.presentation.homeScreen.navigation.BottomNavigation
 
 
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@OptIn(ExperimentalMaterial3Api::class)
+@RequiresApi(Build.VERSION_CODES.Q)
 @Composable
-fun PaymentSuccess() {
-    SkillvsmeSuccessScreen(
-        successMessage = "Payment Successful",
-        successInfo = "Keep the momentum going and schedule you first class",
-        buttonText = "Proceed to Payment",
-        buttonOnclickAction = {},
-        backButtonText = "Back",
-        backButtonOnclickAction = {},
+fun PaymentSuccess(
+    navController: NavController
+) {
+    Scaffold(
+        topBar = {
+            SimpleAppBar(navController = navController, text = "Confirmation")
+        },
+        content = {
+            Spacer(modifier = Modifier.height(40.dp))
+            SkillvsmeSuccessScreen(
+                successMessage = "Payment Successful",
+                successInfo = "Keep the momentum going and schedule you first class",
+                buttonText = "Proceed to Payment",
+                buttonOnclickAction = {
+                    navController.navigate(Route.Student.Tutor.Schedule)
+                },
+                backButtonText = "Back",
+                backButtonOnclickAction = {
+                    navController.popBackStack()
+                },
+            )
+            Spacer(modifier = Modifier.height(40.dp))
+        },
+        bottomBar = {
+            BottomNavigation(navController = navController)
+        }
     )
 }
