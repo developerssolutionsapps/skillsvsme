cb374e2521fbcfa2771ac9adec3a624c1ef1c6cb
daviekim <daviekim.dev@gmail.com>
2024-02-20 01:56:54 -0600

    Skill vs me schedule timeline uses the datecard moved in the new file

--- app/src/main/java/s/skillvsme/presentation/components/SkillvsmeScheduleTimeline.kt
+++ app/src/main/java/s/skillvsme/presentation/components/SkillvsmeScheduleTimeline.kt
@@ -30,51 +30,7 @@ fun SkillvsmeScheduleTimeline(
     selectedTime: String,
 ) {
     Row {
-        Box(
-            modifier = Modifier
-                .padding(top = 24.dp, end = 12.dp)
-                .fillMaxWidth(0.2f)
-                .background(
-                    color = lightGrey,
-                    shape = RoundedCornerShape(12.dp)
-                )
-        ) {
-            Column() {
-                Spacer(modifier = Modifier.height(8.dp))
-                Text(
-                    fontFamily = Fonts.jostFontFamily,
-                    text = day,
-                    fontWeight = FontWeight.SemiBold,
-                    color = purple,
-                    fontSize = 18.sp,
-                    modifier = Modifier
-                        .padding(horizontal = 8.dp)
-                        .fillMaxWidth(),
-                    textAlign = TextAlign.Center
-                )
-                Text(
-                    fontFamily = Fonts.jostFontFamily,
-                    text = date,
-                    fontWeight = FontWeight.SemiBold,
-                    color = purple,
-                    fontSize = 18.sp,
-                    modifier = Modifier
-                        .padding(horizontal = 8.dp)
-                        .fillMaxWidth(),
-                    textAlign = TextAlign.Center
-                )
-                Text(
-                    fontFamily = Fonts.jostFontFamily,
-                    text = month,
-                    fontWeight = FontWeight.Bold,
-                    fontSize = 18.sp,
-                    modifier = Modifier
-                        .padding(horizontal = 8.dp)
-                        .fillMaxWidth(),
-                    textAlign = TextAlign.Center
-                )
-            }
-        }
+        DateCard(day = day, date = date, month = month)
         Column {
             for (time in timelineList){
                 Spacer(modifier = Modifier.height(8.dp))
