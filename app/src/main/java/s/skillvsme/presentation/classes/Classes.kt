9f0b3eeba0f6f2b7dabb333b26c28a8f1450e9c5
daviekim <daviekim.dev@gmail.com>
2024-02-20 21:50:22 -0600

    implements the ui for the classes screen

--- app/src/main/java/s/skillvsme/presentation/classes/Classes.kt
+++ app/src/main/java/s/skillvsme/presentation/classes/Classes.kt
@@ -2,8 +2,11 @@ package s.skillvsme.presentation.classes
 
 import android.annotation.SuppressLint
 import android.os.Build
+import androidx.activity.compose.BackHandler
 import androidx.annotation.RequiresApi
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -12,63 +15,259 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.BottomSheetScaffold
+import androidx.compose.material.BottomSheetState
+import androidx.compose.material.BottomSheetValue
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.rememberBottomSheetScaffoldState
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import kotlinx.coroutines.launch
+import s.skillvsme.R
+import s.skillvsme.common.Fonts
 import s.skillvsme.common.Route
+import s.skillvsme.presentation.components.PastClassesItem
 import s.skillvsme.presentation.components.SimpleAppBar
 import s.skillvsme.presentation.components.SkillvsmeButton
-import s.skillvsme.presentation.components.SkillvsmeSuccessScreen
+import s.skillvsme.presentation.components.SkillvsmeRadioBtn
+import s.skillvsme.presentation.components.SkillvsmeText
+import s.skillvsme.presentation.components.UpComingClassesItem
 import s.skillvsme.presentation.homeScreen.navigation.BottomNavigation
+import s.skillvsme.ui.theme.black
 import s.skillvsme.ui.theme.lightGrey
+import s.skillvsme.ui.theme.red
 
 @RequiresApi(Build.VERSION_CODES.Q)
-@OptIn(ExperimentalMaterial3Api::class)
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalMaterialApi::class)
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun Classes(
     navController:NavController
 ){
-    Scaffold(
-        topBar = {
-            SimpleAppBar(navController = navController, text = "Checkout")
-        },
-        content = { paddingValues ->
-            Column(
-                modifier = Modifier
-                    .padding(paddingValues)
-                    .padding(24.dp)
-                    .fillMaxSize(),
-                verticalArrangement = Arrangement.SpaceBetween
-            ) {
-                Surface(
-                    modifier = Modifier
-                        .fillMaxWidth(),
-                    shape = RoundedCornerShape(20.dp)
-                ) {
-                    Row(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .background(lightGrey)
-                    ) {
-                        SkillvsmeButton(
-                            label = "Upcoming"
-                        )
-                        SkillvsmeButton(
-                            label = "Upcoming"
-                        )
-                    }
+    var upcomingSelected by remember { mutableStateOf(true) }
+    val scope = rememberCoroutineScope()
+    val density = LocalDensity.current
+    val bottomSheetScaffoldState = rememberBottomSheetScaffoldState(
+        bottomSheetState = BottomSheetState(
+            initialValue = BottomSheetValue.Collapsed,
+            density = density
+        )
+    )
+    BackHandler {
+        if (bottomSheetScaffoldState.bottomSheetState.isExpanded) {
+            scope.launch {
+                bottomSheetScaffoldState.bottomSheetState.collapse()
+            }
+        } else {
+            // Navigate back or handle back button press in another way
+            navController.popBackStack()
+        }
+    }
+    BottomSheetScaffold(
+        scaffoldState = bottomSheetScaffoldState,
+        sheetPeekHeight = 0.dp,
+        sheetContent = {
+            CancelBottomSheet(navController = navController)
+            LaunchedEffect(key1 = Unit) {
+                scope.launch {
+                    bottomSheetScaffoldState.bottomSheetState.collapse()
                 }
             }
         },
-        bottomBar = {
-            BottomNavigation(navController = navController)
+        sheetBackgroundColor = Color.Gray.copy(alpha = 0.5f), // Set sheet background color
+        modifier = Modifier.background(Color.Gray.copy(alpha = 0.5f))
+    ) {
+        Scaffold(
+            topBar = {
+                SimpleAppBar(navController = navController, text = "Checkout")
+            },
+            content = { paddingValues ->
+                Column(
+                    modifier = Modifier
+                        .padding(paddingValues)
+                        .padding(20.dp)
+                        .fillMaxSize()
+                        .verticalScroll(rememberScrollState()),
+                    verticalArrangement = Arrangement.SpaceBetween,
+                ) {
+                    Spacer(modifier = Modifier.height(8.dp))
+                    Surface(
+                        modifier = Modifier
+                            .fillMaxWidth(),
+                        shape = RoundedCornerShape(20.dp)
+                    ) {
+                        Row {
+                            Surface(
+                                modifier = Modifier
+                                    .weight(1f)
+                                    .clickable {
+                                        upcomingSelected = true
+                                    },
+                                shape = RoundedCornerShape(24.dp),
+                                color = if (upcomingSelected) black else lightGrey
+                            ) {
+                                Text(
+                                    modifier = Modifier
+                                        .padding(12.dp)
+                                        .fillMaxWidth(),
+                                    text = "Upcoming",
+                                    textAlign = TextAlign.Center,
+                                    fontFamily = Fonts.jostFontFamily
+                                )
+                            }
+                            Spacer(modifier = Modifier.width(8.dp))
+                            Surface(
+                                modifier = Modifier
+                                    .weight(1f)
+                                    .background(lightGrey)
+                                    .clickable {
+                                        upcomingSelected = false
+                                    },
+                                shape = RoundedCornerShape(24.dp),
+                                color = if (upcomingSelected) lightGrey else black
+                            ) {
+                                Text(
+                                    modifier = Modifier
+                                        .padding(12.dp)
+                                        .fillMaxWidth(),
+                                    text = "Past",
+                                    textAlign = TextAlign.Center,
+                                    fontFamily = Fonts.jostFontFamily
+                                )
+                            }
+                        }
+                    }
+                    Spacer(modifier = Modifier.height(20.dp))
+                    repeat(6) {
+                        if (upcomingSelected) {
+                            UpComingClassesItem(
+                                day = "Wed",
+                                date = "29",
+                                month = "Nov",
+                                name = "Alexander Brik",
+                                location = "California, USA | 6:30 PM",
+                                image = "",
+                                rating = 5.0,
+                                time = "10:00 - 11:00 AM",
+                                cancelClicked = {
+                                    scope.launch {
+                                        bottomSheetScaffoldState.bottomSheetState.expand()
+                                    }
+                                }
+                            )
+                        } else {
+                            PastClassesItem(
+                                day = "Wed",
+                                date = "29",
+                                month = "Nov",
+                                name = "Alexander Brik",
+                                location = "California, USA | 6:30 PM",
+                                image = "",
+                                rating = 5.0,
+                                time = "10:00 - 11:00 AM",
+                                watchClicked = {
+                                    navController.navigate(Route.Student.Classes.ClassRoom)
+                                }
+                            )
+                        }
+                        Spacer(
+                            modifier = Modifier
+                                .height(16.dp)
+                        )
+                    }
+                }
+            },
+            bottomBar = {
+                BottomNavigation(navController = navController)
+            }
+        )
+    }
+}
+
+@Composable
+fun CancelBottomSheet(navController: NavController) {
+    Surface(
+        shape = RoundedCornerShape(topStart = 20.dp, topEnd = 20.dp)
+    ){
+        Column(
+            modifier = Modifier
+                .padding(20.dp)
+                .fillMaxWidth()
+        ){
+            Row(
+                modifier = Modifier
+                    .padding(4.dp)
+                    .fillMaxWidth(),
+                horizontalArrangement = Arrangement.End
+            ){
+                Image(
+                    modifier = Modifier
+                        .clickable {
+                            navController.popBackStack()
+                        },
+                    painter = painterResource(id = R.drawable.cancel),
+                    contentDescription = null
+                )
+            }
+            SkillvsmeText(
+                iconBefore = painterResource(id = R.drawable.warning),
+                valueColor = red,
+                value = "Please note that your maximum limit is 2 classes/month. If you cancel this class you will remain with only 1 cancelation left"
+            )
+            SkillvsmeRadioBtn(
+                modifier = Modifier,
+                selectedValue = "I booked by mistake",
+                label = "I booked by mistake"
+            )
+            SkillvsmeRadioBtn(
+                modifier = Modifier,
+                selectedValue = "I booked by mistake",
+                label = "I have change of plan"
+            )
+            SkillvsmeRadioBtn(
+                modifier = Modifier,
+                selectedValue = "I booked by mistake",
+                label = "I have change of plan"
+            )
+            SkillvsmeRadioBtn(
+                modifier = Modifier,
+                selectedValue = "I booked by mistake",
+                label = "I have change of plan"
+            )
+            SkillvsmeRadioBtn(
+                modifier = Modifier,
+                selectedValue = "I booked by mistake",
+                label = "I have change of plan"
+            )
+            SkillvsmeRadioBtn(
+                modifier = Modifier,
+                selectedValue = "I booked by mistake",
+                label = "I have change of plan"
+            )
+            SkillvsmeButton(
+                label = "Cancel",
+                onClick = {}
+            )
         }
-    )
+    }
 }
