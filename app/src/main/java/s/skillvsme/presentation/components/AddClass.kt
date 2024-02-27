6e8fa4f194141aa1185fe53b83da8026e889dc0b
daviekim <daviekim.dev@gmail.com>
2024-02-27 07:57:23 -0600

    adds a component for adding the classes

--- app/src/main/java/s/skillvsme/presentation/components/AddClass.kt
+++ app/src/main/java/s/skillvsme/presentation/components/AddClass.kt
@@ -5,8 +5,10 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -14,15 +16,17 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
 import s.skillvsme.R
 import s.skillvsme.common.Fonts
+import s.skillvsme.common.Route
 import s.skillvsme.ui.theme.black
 import s.skillvsme.ui.theme.white
 
 @Composable
 fun AddClass(
     modifier: Modifier = Modifier,
-    onClick: () -> Unit = {}
+    navController: NavController
 ) {
     val primary = false
     BorderedSurface(
@@ -39,11 +43,12 @@ fun AddClass(
                     color = if (primary) black else white
                 )
                 .clickable {
-                    onClick
+                    navController.navigate(Route.Tutor.Classes.AddClass)
                 }
                 .padding(vertical = 8.dp, horizontal = 20.dp),
         ){
             Image(painter = painterResource(id = R.drawable.add_black_bg), contentDescription = null)
+            Spacer(modifier = Modifier.width(20.dp))
             Text(
                 text = "Add a class",
                 color = if (primary) white else black,
