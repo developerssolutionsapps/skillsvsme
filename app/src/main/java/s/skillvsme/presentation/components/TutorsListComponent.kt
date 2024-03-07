bd9adaff3f0eee71522ef5f6752717756817ba79
AriqKip <ericokip402@gmail.com>
2024-03-07 08:37:29 +0300

    adjusting the tutors list in students list

--- app/src/main/java/s/skillvsme/presentation/components/TutorsListComponent.kt
+++ app/src/main/java/s/skillvsme/presentation/components/TutorsListComponent.kt
@@ -1,6 +1,8 @@
 package s.skillvsme.presentation.components
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -10,8 +12,11 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Divider
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -23,6 +28,8 @@ import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import s.skillvsme.R
 import s.skillvsme.common.Route
+import s.skillvsme.ui.theme.black
+import s.skillvsme.ui.theme.purple
 
 
 @Composable
@@ -34,20 +41,25 @@ fun TutorsListComponent(
     isLive: Boolean = true,
     navController: NavController
 ) {
+
     Row(
         modifier = Modifier
             .clickable {
                 navController.navigate(Route.Student.Tutor.TutorProfile)
             }
-    ){
+            .fillMaxWidth()
+            .wrapContentHeight(),
+
+        ) {
         Column {
+            Spacer(modifier = Modifier.height(24.dp))
             Box(
                 modifier = Modifier
-                    .height(100.dp)
-                    .width(80.dp),
+                    .height(92.dp)
+                    .width(76.dp),
             ) {
                 Surface(
-                    shape = RoundedCornerShape(24.dp)
+                    shape = RoundedCornerShape(20.dp)
                 ) {
                     Image(
                         painter = profile,
@@ -64,9 +76,10 @@ fun TutorsListComponent(
                 )
             }
         }
+        Spacer(modifier = Modifier.width(4.dp))
         Column(
             modifier = Modifier
-                .padding(horizontal = 12.dp)
+                .padding(start = 12.dp, end = 12.dp, top = 30.dp)
         ) {
             Row(
                 modifier = Modifier
@@ -82,17 +95,30 @@ fun TutorsListComponent(
                 Row(
                     verticalAlignment = Alignment.CenterVertically
                 ) {
-                    Image(painter = painterResource(R.drawable.rate_star), contentDescription = "rate star")
+                    Image(
+                        painter = painterResource(R.drawable.rate_star),
+                        modifier = Modifier.size(20.dp),
+                        contentDescription = "rate star",
+                    )
                     SkillvsmeText(
                         value = "${rating}",
-                        valueSize = 16
+                        valueSize = 14
                     )
                 }
             }
+            Spacer(modifier = Modifier.height(4.dp))
             SkillvsmeText(
                 value = bio,
+                maxline = 4,
+                lineheight = 1.13,
+                letterSpacing = 0.02,
                 valueSize = 16
             )
+            Spacer(modifier = Modifier.height(12.dp))
         }
     }
+    Divider(
+        thickness = 1.dp,
+        color = black
+    )
 }
