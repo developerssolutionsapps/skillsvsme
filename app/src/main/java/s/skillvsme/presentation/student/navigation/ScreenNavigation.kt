a678aa4378a96854343f646d28a1eaf9964b9576
AriqKip <ericokip402@gmail.com>
2024-03-14 08:11:14 +0300

    fixing the live streaming screen

--- app/src/main/java/s/skillvsme/presentation/student/navigation/ScreenNavigation.kt
+++ app/src/main/java/s/skillvsme/presentation/student/navigation/ScreenNavigation.kt
@@ -4,7 +4,7 @@ import s.skillvsme.R
 sealed class ScreenNavigation (var title :String,var icon :Int,var screenroute :String){
     object Home : ScreenNavigation("Home", R.drawable.home ,"/student/home")
     object Tutor : ScreenNavigation("Tutor", R.drawable.teachersvg,"/student/tutor/tutorslist")
-    object Stream : ScreenNavigation("Stream",R.drawable.live_broadcasting,"/student/streaming/livestream")
+    object Stream : ScreenNavigation("Stream",R.drawable.live_broadcasting,"/student/streaming/viewlivestream")
     object Classes : ScreenNavigation("Classes",R.drawable.schedule_1,"/student/classes/upcomingclasses")
     object Account : ScreenNavigation("Account",R.drawable.account,"/student/profile/studentprofile")
 }
